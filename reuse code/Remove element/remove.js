b3f8641e8f55aab72c81dba75a507b02b624ea90
Nikhil Dharendra <103980416+dharnikh@users.noreply.github.com>
2022-06-25 22:36:40 +0530

    Update remove.js
    just trying if I can edit directly on git or not

--- reuse code/Remove element/remove.js
+++ reuse code/Remove element/remove.js
@@ -9,4 +9,6 @@ function Remove(ele,index){
     localStorage.setItem("key_name",JSON.stringify(Array))
     window.location.reload()
 }
-// Splice function will take two arguements first arguement is starting index and second arguement is delete count
+// Splice function will take two arguements first arguement is starting index and second arguement is delete count 
+// 25 june 2022 10:35 pm 
+// trying if i can can commit changes directly to git or not and yes i can 
